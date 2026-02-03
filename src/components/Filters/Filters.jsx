09a0f15d9dfb0d938b553e2bca2b12aed3acec5e
Alexey <a03221983@gmail.com>
2026-02-03 17:47:54 +0200

    898

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -19,81 +19,70 @@ export default function Filters() {
         className={styles.input}
         placeholder="Kyiv, Ukraine"
         value={filters.location}
-        onChange={(e) => dispatch(setLocation(e.target.value))}
+        onChange={e => dispatch(setLocation(e.target.value))}
       />
 
-      <p className={styles.title}>Filters</p>
+<p className={styles.title}>Filters</p>
 
-      {/* Vehicle equipment */}
-      <div className={styles.block}>
-        <p className={styles.subtitle}>Vehicle equipment</p>
-        <div className={styles.grid}>
-          <label className={styles.card}>
-            <input
-              type="checkbox"
-              onChange={() => dispatch(toggleFeature("AC"))}
-            />
-            <img src="/icons/ac.svg" />
-            <span>AC</span>
-          </label>
+{/* Vehicle equipment */}
+<div className={styles.block}>
+  <p className={styles.subtitle}>Vehicle equipment</p>
+  <div className={styles.grid}>
+    <label className={styles.card}>
+      <input type="checkbox" />
+      <img src="/icons/ac.svg" alt="AC" />
+      <span>AC</span>
+    </label>
 
-          <label className={styles.card}>
-            <input type="checkbox" />
-            <img src="/public/icons/automatic.svg" alt="Automatic" />
-            <span>Automatic</span>
-          </label>
+    <label className={styles.card}>
+      <input type="checkbox" />
+      <img src="/icons/automatic.svg" alt="Automatic" />
+      <span>Automatic</span>
+    </label>
 
-          <label className={styles.card}>
-            <input type="checkbox" />
-            <img src="/public/icons/kitchen.svg" alt="Kitchen" />
-            <span>Kitchen</span>
-          </label>
+    <label className={styles.card}>
+      <input type="checkbox" />
+      <img src="/icons/kitchen.svg" alt="Kitchen" />
+      <span>Kitchen</span>
+    </label>
 
-          <label className={styles.card}>
-            <input type="checkbox" />
-            <img src="/public/icons/tv.svg" alt="TV" />
-            <span>TV</span>
-          </label>
+    <label className={styles.card}>
+      <input type="checkbox" />
+      <img src="/icons/tv.svg" alt="TV" />
+      <span>TV</span>
+    </label>
 
-          <label className={styles.card}>
-            <input type="checkbox" />
-            <img src="/public/icons/bathroom.svg" alt="Bathroom" />
-            <span>Bathroom</span>
-          </label>
-        </div>
-      </div>
+    <label className={styles.card}>
+      <input type="checkbox" />
+      <img src="/icons/bathroom.svg" alt="Bathroom" />
+      <span>Bathroom</span>
+    </label>
+  </div>
+</div>
 
-      {/* Vehicle type */}
-      <div className={styles.block}>
-        <p className={styles.subtitle}>Vehicle type</p>
-        <div className={styles.grid}>
-          <label className={styles.card}>
-            <input
-              type="radio"
-              name="type"
-              onChange={() => dispatch(setBodyType("Van"))}
-            />
-            <img src="/icons/van.svg" />
-            <span>Van</span>
-          </label>
+{/* Vehicle type */}
+<div className={styles.block}>
+  <p className={styles.subtitle}>Vehicle type</p>
+  <div className={styles.grid}>
+    <label className={styles.card}>
+      <input type="radio" name="type" defaultChecked/>
+      <img src="/icons/van.svg" alt="Van" />
+      <span>Van</span>
+    </label>
 
-          <label className={styles.card}>
-            <input
-              type="radio"
-              name="type"
-              onChange={() => dispatch(setBodyType("Fully Integrated"))}
-            />
-            <img src="/icons/full.svg" />
-            <span className={styles.fully}>Fully Integrated</span>
-          </label>
+    <label className={styles.card}>
+      <input type="radio" name="type"/>
+      <img src="/icons/full.svg" alt="Fully Integrated" />
+      <span  className={styles.fully}>Fully <br />Integrated</span>
+    </label>
 
-          <label className={styles.card}>
-            <input type="radio" name="type" />
-            <img src="/public/icons/alcove.svg" alt="Alcove" />
-            <span>Alcove</span>
-          </label>
-        </div>
-      </div>
+    <label className={styles.card}>
+      <input type="radio" name="type" />
+      <img src="/icons/alcove.svg" alt="Alcove" />
+      <span>Alcove</span>
+    </label>
+  </div>
+</div>
 
       <button className={styles.search} onClick={handleSearch}>
         Search
